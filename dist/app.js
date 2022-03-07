86f4abd3e6513d834b250f6c01d75616364b2d05
hung15102001 <82076329+hung15102001@users.noreply.github.com>
2022-03-07 15:14:02 +0700

    a

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function test(a, b) {
+    return a + b;
+}
+const kq = test(40, 20);
+console.log(kq);
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+function test(a: number, b: number): number {
+    return a+b;
+}
+
+const kq = test(40, 20);
+console.log(kq);
