a9b8f5d8dc13d11b5be63ccbd0e8e803f6b61543
hung15102001 <82076329+hung15102001@users.noreply.github.com>
2022-03-10 11:34:05 +0700

    a

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,16 +6,23 @@ import ShowInfo from './components/ShowInfo'
 
 function App() {
   const [count, setCount] = useState(0)
-  const products = [
-    {id: 1, name:"name a"},
-    {id: 2, name:"name b"},
-    {id: 3, name:"name c"}
-  ];
+ 
+const [products, setProducts] = useState([
+  {id: 1, name:"name a"},
+  {id: 2, name:"name b"},
+  {id: 3, name:"name c"}
+])
 
+  const removeItem = (id) =>{
+    const newProduct = products.filter(item => item.id !== id);
+    setProducts(newProduct);
+  }
   return (
     <div className="App">
       Count: {count} <button onClick={()=> setCount(count+1)}>Click</button>
-      {products.map((item, index) => <Item key={index} data={item}/>)}
+      {products.map((item, index) => <div>{item.name}:<button onClick={()=> removeItem(item.id)}>Remove</button></div>
+        
+      )}
       <ShowInfo name="Hung" age={29}/>
     </div>
   )
